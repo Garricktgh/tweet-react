e99b115da8a9088015d57ed913feca2c96e42782
garricktgh <garrick@hotmail.sg>
2019-11-20 17:09:24 +0800

    add css to app

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -10,10 +10,22 @@ class App extends React.Component {
       return <Tweet name={item["user"]["name"]} screenName={item["user"]["screen_name"]} createdAt={item["created_at"]} retweetCount={item["retweet_count"]} favoriteCount={item["favorite_count"]} text={item["text"]}></Tweet>;
     });
     return (
-      <div>
-        tweets
-        {tweetItems}
-      </div>
+      <html>
+          <head>
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossOrigin="anonymous"></link>
+            <link rel="stylesheet" href="/style.css"></link>
+            <link href='https://cdn.jsdelivr.net/npm/boxicons@2.0.3/css/boxicons.min.css' rel='stylesheet'></link>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.bundle.js"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.bundle.min.js"></script>
+            <title>Tweeter</title>
+          </head>
+          <body>
+            <div>
+              tweets
+              {tweetItems}
+            </div>
+          </body>
+      </html>
     );
   }
 }
